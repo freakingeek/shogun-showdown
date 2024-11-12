e651713cb02a35931fbdc006745b11c29490d268
Yasin Silavi <ys.silavi@gmail.com>
2024-11-12 18:57:28 +0330

    feat: implement single-post component WIP

---
+++ src/components/post/SinglePost.tsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+
+type SinglePostProps = {
+  id: number;
+  title: string;
+  author: string;
+  date: string;
+  description: string;
+};
+
+export default function SinglePost({
+  id,
+  title,
+  author,
+  date,
+  description,
+}: SinglePostProps) {
+  const postUrl = `/post/${id}`;
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center gap-x-2">
+        <span className="text-sm font-medium">{author}</span>
+        <span className="w-1 h-1 bg-white rounded-sm"></span>
+        <span className="text-xs">{date}</span>
+      </div>
+
+      <Link
+        to={postUrl}
+        className="text-xl font-bold mt-1 transition-colors hover:text-primary"
+      >
+        {title}
+      </Link>
+
+      <p className="font-sans mt-4">{description}</p>
+
+      <Link to={postUrl} className="text-primary mt-8">
+        Read more
+      </Link>
+    </div>
+  );
+}
