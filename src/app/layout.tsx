00a4ca5013fa33349fc62ba4368d8063a03ae5d6
Yasin Silavi <ys.silavi@gmail.com>
2024-11-12 04:55:43 +0330

    feat: set project container to `1280px`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
-  return <Outlet />;
+  return (
+    <section className="max-w-screen-xl mx-auto">
+      <Outlet />
+    </section>
+  );
 }
