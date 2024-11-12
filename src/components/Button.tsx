f415d9c843fd6337e76c72736a930d4f896df97f
Yasin Silavi <ys.silavi@gmail.com>
2024-11-12 18:56:17 +0330

    feat: implement button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,43 @@
+import classNames from "classnames";
+import { PropsWithChildren } from "react";
+import { Link } from "react-router-dom";
+
+type ButtonProps = PropsWithChildren<{
+  href?: string;
+  className?: string;
+  variant?: "primary" | "secondary";
+  onClick?: VoidFunction;
+}>;
+
+export default function Button({
+  href,
+  children,
+  className,
+  variant = "primary",
+  ...rest
+}: ButtonProps) {
+  const styles: Record<typeof variant, string> = {
+    primary: "bg-primary border-primary text-white",
+    secondary: "bg-black border-primary text-primary",
+  };
+
+  const mergedClassNames = classNames(
+    "flex items-center justify-center h-12 font-bold rounded-lg border",
+    styles[variant],
+    className
+  );
+
+  if (href) {
+    return (
+      <Link to={href} className={mergedClassNames}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button {...rest} className={mergedClassNames}>
+      {children}
+    </button>
+  );
+}
