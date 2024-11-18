aabc7d2089b281541c7f6b1a3ebce94e91282b53
Yasin Silavi <ys.silavi@gmail.com>
2024-11-18 17:19:32 +0330

    feat: add email validation mutation

---
+++ src/graphql/mutations/emailValidation.ts
@@ -0,0 +1,24 @@
+import { gql, TypedDocumentNode } from "@apollo/client";
+
+type EmailValidationData = {
+  validateEmail: {
+    valid: boolean;
+  };
+};
+
+type EmailValidationVariables = {
+  input: {
+    email: string;
+  };
+};
+
+export const EMAIL_VALIDATION_MUTATION: TypedDocumentNode<
+  EmailValidationData,
+  EmailValidationVariables
+> = gql`
+  mutation AuthFormValidateEmailMutation($input: RequestGlobalTokenInput!) {
+    validateEmail(input: $input) {
+      valid
+    }
+  }
+`;
