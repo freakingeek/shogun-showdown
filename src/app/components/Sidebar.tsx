c56c09e58e0f087e37cdcf60d5c2d3bae0dcb986
Yasin Silavi <ys.silavi@gmail.com>
2024-11-18 13:50:54 +0330

    feat: update sidebar to show user profile on login

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,19 +1,27 @@
 import Menu from "./Menu";
 import Button from "@/components/Button";
+import Profile from "@/components/Profile";
+import { useAuthContext } from "@/providers/AuthProvider";
 
 type SidebarProps = {
   className?: string;
 };
 
 export default function Sidebar({ className }: SidebarProps) {
+  const { isLoggedIn } = useAuthContext();
+
   return (
     <aside className={className}>
       <div className="sticky top-4">
         <Menu />
 
-        <Button href="/accounts/login" className="w-full mt-32">
-          Login
-        </Button>
+        {isLoggedIn ? (
+          <Profile />
+        ) : (
+          <Button href="/accounts/login" className="w-full mt-32">
+            Login
+          </Button>
+        )}
       </div>
     </aside>
   );
