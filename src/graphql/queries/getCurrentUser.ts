ac95a7ff8613b4998795386a8ce1146f48222f3e
Yasin Silavi <ys.silavi@gmail.com>
2024-11-18 23:56:12 +0330

    feat: add get-current-user query

---
+++ src/graphql/queries/getCurrentUser.ts
@@ -0,0 +1,32 @@
+import { gql, TypedDocumentNode } from "@apollo/client";
+
+type Image = {
+  url: string;
+};
+
+type GetCurrentUserData = {
+  authMember: {
+    name: string;
+    email: string;
+    profilePicture: Image | null;
+  };
+};
+
+type GetCurrentUserVariables = {
+  limit: number;
+  offset: number;
+};
+
+export const GET_CURRENT_USER_QUERY: TypedDocumentNode<GetCurrentUserData, GetCurrentUserVariables> = gql`
+  query GetCurrentUser {
+    authMember {
+      name
+      email
+      profilePicture {
+        ... on Image {
+          url
+        }
+      }
+    }
+  }
+`;
