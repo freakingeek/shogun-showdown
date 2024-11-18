e9ec1d91b59a49a2e3eca360f5b5108de195b06f
Yasin Silavi <ys.silavi@gmail.com>
2024-11-19 00:00:26 +0330

    feat: get user data from the server

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,27 @@
+import classNames from "classnames";
+import { useQuery } from "@apollo/client";
+import { GET_CURRENT_USER_QUERY } from "@/graphql/queries/getCurrentUser";
+
 export default function Profile() {
+  const { data } = useQuery(GET_CURRENT_USER_QUERY);
+
   return (
     <div className="flex gap-x-2 bg-dark py-2 px-4 rounded-lg">
-      <div className="w-12 h-12 flex items-center justify-center bg-white rounded-md shrink-0">
+      <div
+        className={classNames("w-12 h-12 flex items-center justify-center rounded-md shrink-0", {
+          "bg-white": !data?.authMember.profilePicture?.url,
+        })}
+      >
         <img
-          src="/assets/images/profile.png"
-          alt="Avatar of Yasin Silavi"
+          src={data?.authMember.profilePicture?.url || "/assets/images/profile.png"}
+          alt={`Avatar of ${data?.authMember.name}`}
           className="rounded-md"
         />
       </div>
 
       <div className="w-40 truncate text-secondary">
-        <h3 className="text-white">Yasin Silavi</h3>
-        <span className="text-xs">ys.silavi@gmail.com</span>
+        <h3 className="text-white">{data?.authMember.name}</h3>
+        <span className="text-xs">{data?.authMember.email}</span>
       </div>
     </div>
   );
