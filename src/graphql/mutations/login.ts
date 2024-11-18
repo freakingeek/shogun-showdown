ca169c915ad3b002924a6801797d5842fcc45b22
Yasin Silavi <ys.silavi@gmail.com>
2024-11-18 23:58:45 +0330

    feat: add login mutation

---
+++ src/graphql/mutations/login.ts
@@ -0,0 +1,28 @@
+import { gql, TypedDocumentNode } from "@apollo/client";
+
+type LoginData = {
+  loginNetwork: {
+    accessToken: string;
+    member: {
+      name: string;
+      email: string;
+    };
+  };
+};
+
+type LoginVariables = {
+  email: string;
+  password: string;
+};
+
+export const LOGIN_MUTATION: TypedDocumentNode<LoginData, LoginVariables> = gql`
+  mutation login($email: String!, $password: String!) {
+    loginNetwork(input: { usernameOrEmail: $email, password: $password }) {
+      accessToken
+      member {
+        name
+        email
+      }
+    }
+  }
+`;
