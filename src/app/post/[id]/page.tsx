7abed522fe28777401e6adf2eb76932bbda1e18e
Yasin Silavi <ys.silavi@gmail.com>
2024-11-20 01:34:22 +0330

    feat: get post data from the server

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,68 +1,58 @@
+import dayjs from "dayjs";
 import Like from "@/components/Like";
-import { Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { Link, useParams } from "react-router-dom";
 import MainLayout from "@/components/layouts/Main";
+import { NotFoundError } from "@/lib/exceptions/instances";
+import { GET_SINGLE_POST_QUERY } from "@/graphql/queries/getSinglePost";
 
 export default function SinglePostPage() {
+  const params = useParams<{ id: string }>();
+  const { data, loading, error } = useQuery(GET_SINGLE_POST_QUERY, { variables: { id: params.id! } });
+
+  if (loading) {
+    return (
+      <MainLayout>
+        <span>Loading ...</span>
+      </MainLayout>
+    );
+  }
+
+  if (error) {
+    return (
+      <MainLayout>
+        <span>{error?.message || "Something bad happened!"}</span>
+      </MainLayout>
+    );
+  }
+
+  if (!loading && !data) {
+    throw new NotFoundError();
+  }
+
+  const content = data?.post.fields.find((field) => field.key === "content")?.value || "";
+  const slicedContent = content.slice(1, content.length - 1); // "Content" => Content
+
   return (
     <MainLayout>
-      <article className="flex flex-col">
+      <div className="flex flex-col">
         <div className="flex items-center gap-x-2">
           <span className="text-sm font-medium">Yasin Silavi</span>
-          <span className="w-1 h-1 bg-secondary rounded-sm"></span>
-          <span className="text-xs">09 Days ago</span>
+          <span className="w-1 h-1 bg-[#d9d9d9] rounded-sm"></span>
+          <span className="text-xs text-secondary">{dayjs(data?.post.createdAt).fromNow()}</span>
         </div>
 
-        <Link
-          to={"/"}
-          className="text-xl font-bold mt-1 transition-colors hover:text-primary"
-        >
-          New Maps are just added to the game! Let’s see the mysteries they
-          brought to us!
+        <Link to={`/post/${params.id}`} className="text-xl font-bold mt-1 transition-colors hover:text-primary">
+          {data?.post.title}
         </Link>
 
-        <p className="font-sans mt-4">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
-          vero eros et accumsan et iusto odio dignissim qui blandit ortis nisl
-          ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in
-          hendrerit in vulputate velit esse molestie consequat, vel illum dolore
-          eu feugiat nulla facilisis at vero eros et accumsan et iusto odio
-          dignissim qui blandit ortis nisl ut aliquip ex ea commodo consequat.
-          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
-          vero eros et accumsan et iusto odio dignissim qui blandit ortis nisl
-          ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in
-          hendrerit in vulputate velit esse molestie consequat, vel illum dolore
-          eu feugiat nulla facilisis at vero eros et accumsan et iusto odio
-          dignissim qui blandit ortis nisl ut aliquip ex ea commodo consequat.
-          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
-          vero eros et accumsan et iusto odio dignissim qui blandit praesent
-          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-          <br />
-          <br />
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
-          vero eros et accumsan et iusto odio dignissim qui blandit praesent
-          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-        </p>
+        <article
+          dangerouslySetInnerHTML={{ __html: slicedContent }}
+          className="prose prose-headings:text-white prose-strong:text-white prose-p:text-secondary prose-a:text-primary font-sans mt-4"
+        />
 
-        <Like className="mt-8" />
-      </article>
+        <Like likesCount={data?.post.reactionsCount} isLiked={data?.post.reactions.reacted} className="mt-8" />
+      </div>
     </MainLayout>
   );
 }
