adebc3e65620c3a9396ac71d3b88861a7b85c7d8
Yasin Silavi <ys.silavi@gmail.com>
2024-11-20 00:26:11 +0330

    feat: fetch posts list from the server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,71 +1,70 @@
 import Post from "@/components/Post";
 import Button from "@/components/Button";
+import { useQuery } from "@apollo/client";
 import MainLayout from "@/components/layouts/Main";
+import { GET_POSTS_LIST_QUERY } from "@/graphql/queries/getPostsList";
+
+const POSTS_PER_REQUEST = 5;
 
 export default function HomePage() {
-  const posts = [
-    {
-      id: 0,
-      title: `New Maps are just added to the game! Let’s see the mysteries they brought to us!`,
-      author: "Yasin Silavi",
-      date: "10 Days ago",
-      description: `This file contains additional information, probably added from the digital camera or scanner used to create or digitize it. If the file has been modified from its original state, some details may not fully reflect the modified file.`,
-    },
-    {
-      id: 1,
-      title: `New Maps are just added to the game! Let’s see the mysteries they brought to us!`,
-      author: "Yasin Silavi",
-      date: "10 Days ago",
-      description: `This file contains additional information, probably added from the digital camera or scanner used to create or digitize it. If the file has been modified from its original state, some details may not fully reflect the modified file.`,
-    },
-    {
-      id: 2,
-      title: `New Maps are just added to the game! Let’s see the mysteries they brought to us!`,
-      author: "Yasin Silavi",
-      date: "10 Days ago",
-      description: `This file contains additional information, probably added from the digital camera or scanner used to create or digitize it. If the file has been modified from its original state, some details may not fully reflect the modified file.`,
-    },
-    {
-      id: 3,
-      title: `New Maps are just added to the game! Let’s see the mysteries they brought to us!`,
-      author: "Yasin Silavi",
-      date: "10 Days ago",
-      description: `This file contains additional information, probably added from the digital camera or scanner used to create or digitize it. If the file has been modified from its original state, some details may not fully reflect the modified file.`,
-    },
-    {
-      id: 4,
-      title: `New Maps are just added to the game! Let’s see the mysteries they brought to us!`,
-      author: "Yasin Silavi",
-      date: "10 Days ago",
-      description: `This file contains additional information, probably added from the digital camera or scanner used to create or digitize it. If the file has been modified from its original state, some details may not fully reflect the modified file.`,
-    },
-    {
-      id: 5,
-      title: `New Maps are just added to the game! Let’s see the mysteries they brought to us!`,
-      author: "Yasin Silavi",
-      date: "10 Days ago",
-      description: `This file contains additional information, probably added from the digital camera or scanner used to create or digitize it. If the file has been modified from its original state, some details may not fully reflect the modified file.`,
-    },
-    {
-      id: 6,
-      title: `New Maps are just added to the game! Let’s see the mysteries they brought to us!`,
-      author: "Yasin Silavi",
-      date: "10 Days ago",
-      description: `This file contains additional information, probably added from the digital camera or scanner used to create or digitize it. If the file has been modified from its original state, some details may not fully reflect the modified file.`,
-    },
-  ];
+  const { data, loading, error, fetchMore } = useQuery(GET_POSTS_LIST_QUERY, {
+    variables: { limit: POSTS_PER_REQUEST, offset: 0 },
+  });
+
+  const posts = data?.posts.nodes || [];
+  const totalCount = data?.posts.totalCount || 0;
+
+  const fetchMoreData = () => {
+    fetchMore({
+      variables: { offset: data?.posts.nodes.length || POSTS_PER_REQUEST },
+      updateQuery: (prevResult, { fetchMoreResult }) => {
+        fetchMoreResult.posts.nodes = [...prevResult.posts.nodes, ...fetchMoreResult.posts.nodes];
+
+        return fetchMoreResult;
+      },
+    });
+  };
+
+  if (loading) {
+    return (
+      <MainLayout>
+        <span>Loading ...</span>
+      </MainLayout>
+    );
+  }
+
+  if (error) {
+    return (
+      <MainLayout>
+        <span>{error?.message || "Something bad happened!"}</span>
+      </MainLayout>
+    );
+  }
 
   return (
     <MainLayout>
       {posts.map((post) => (
-        <Post {...post} key={post.id} />
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          date={post.createdAt}
+          description={post.description}
+          author={post.owner.member.name}
+          likesCount={post.reactionsCount}
+          hasMoreContent={post.hasMoreContent}
+        />
       ))}
 
-      <div className="w-full h-40 bg-gradient-to-b from-black/0 via-black/95 via-50% to-black relative z-10 !-mt-40" />
+      {posts.length < totalCount && (
+        <>
+          <div className="w-full h-40 bg-gradient-to-b from-black/0 via-black/95 via-50% to-black relative z-10 !-mt-40" />
 
-      <Button variant="secondary" className="w-full">
-        Load more
-      </Button>
+          <Button variant="secondary" className="w-full" onClick={fetchMoreData}>
+            Load more
+          </Button>
+        </>
+      )}
     </MainLayout>
   );
 }
