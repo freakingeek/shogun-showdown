c18ed21cc13d2f9ed91d1a3008e1ae1115c7c800
Yasin Silavi <ys.silavi@gmail.com>
2024-11-19 17:42:56 +0330

    feat: add dayjs relative time plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
+import dayjs from "dayjs";
 import Providers from "@/providers";
 import { Outlet } from "react-router-dom";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+dayjs.extend(relativeTime);
 
 export default function RootLayout() {
   return (
