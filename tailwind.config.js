4917a9916cb8b4be71886224763d9ca93b765b02
Yasin Silavi <ys.silavi@gmail.com>
2024-11-20 01:32:48 +0330

    feat: add `@tailwindcss/typography` tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindTypographyPlugin from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -18,5 +20,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindTypographyPlugin],
 };
