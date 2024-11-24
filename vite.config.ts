37c4c2a7743a0a4fdd177a5c2c317bc17a842708
Yasin Silavi <ys.silavi@gmail.com>
2024-11-25 01:39:44 +0330

    fix: change base url on the production build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // NOTE: For compatibility with GitHub Pages
+  base: process.env.NODE_ENV !== "development" ? "/shogun-showdown/" : "",
   plugins: [react()],
   resolve: {
     alias: {
