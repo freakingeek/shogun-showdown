45c454863d4449b5796d4749aa16b61e65f8cad5
Yasin Silavi <ys.silavi@gmail.com>
2024-11-27 22:50:28 +0330

    fix: fix flashing white color between navigation

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -27,7 +27,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-black">
         {children}
         <ScrollRestoration />
         <Scripts />
