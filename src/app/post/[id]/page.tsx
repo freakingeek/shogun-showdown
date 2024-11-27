f8b5dfe952f0832e8e46df226fc9c5ed6b93de8b
Yasin Silavi <ys.silavi@gmail.com>
2024-11-27 03:42:40 +0330

    refactor: fetch post data server side

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,42 +1,48 @@
 import dayjs from "dayjs";
 import Like from "@/components/Like";
-import { useQuery } from "@apollo/client";
-import { Link, useParams } from "react-router";
+import { Route } from "./+types/page";
+import { Cookies } from "react-cookie";
 import MainLayout from "@/components/layouts/Main";
+import { apolloClient } from "@/lib/apollo-clients";
+import { ApolloError } from "@apollo/client/index.js";
+import { ACCESS_TOKEN_KEY } from "@/configs/constants";
 import { NotFoundError } from "@/lib/exceptions/instances";
+import { Link, useLoaderData, useParams } from "react-router";
 import { GET_SINGLE_POST_QUERY } from "@/graphql/queries/getSinglePost";
+
+export async function loader({ request, params }: Route.LoaderArgs) {
+  const cookies = new Cookies(request.headers.get("cookie"));
+  const token = cookies.get(ACCESS_TOKEN_KEY);
+
+  try {
+    const { data } = await apolloClient.query({
+      query: GET_SINGLE_POST_QUERY,
+      context: { headers: { Authorization: token ? `Bearer ${token}` : "" } },
+      variables: { id: params.id },
+      fetchPolicy: 'no-cache',
+    });
+
+    return data;
+  } catch (error) {
+    if (error instanceof ApolloError) {
+      // @ts-expect-error - TODO: Handle types later
+      const status = error.cause?.status || 500;
+
+      if (status === 404) {
+        throw new NotFoundError();
+      }
+    }
+  }
+}
 
 export default function SinglePostPage() {
   const params = useParams<{ id: string }>();
-  const { data, loading, error } = useQuery(GET_SINGLE_POST_QUERY, {
-    variables: { id: params.id! },
-    fetchPolicy: "no-cache",
-  });
+  const post = useLoaderData() as Awaited<ReturnType<typeof loader>>;
 
-  if (loading) {
-    return (
-      <MainLayout>
-        <span>Loading ...</span>
-      </MainLayout>
-    );
-  }
-
-  if (error) {
-    return (
-      <MainLayout>
-        <span>{error?.message || "Something bad happened!"}</span>
-      </MainLayout>
-    );
-  }
-
-  if (!loading && !data) {
-    throw new NotFoundError();
-  }
-
-  const content = data?.post.fields.find((field) => field.key === "content")?.value || "";
+  const content = post?.post.fields.find((field) => field.key === "content")?.value || "";
   const slicedContent = content.slice(1, content.length - 1); // "Content" => Content
 
-  const isLikedByCurrentUser = data?.post.reactions.some((reaction) => reaction.reacted);
+  const isLikedByCurrentUser = post?.post.reactions.some((reaction) => reaction.reacted);
 
   return (
     <MainLayout>
@@ -44,11 +50,11 @@ export default function SinglePostPage() {
         <div className="flex items-center gap-x-2">
           <span className="text-sm font-medium">Yasin Silavi</span>
           <span className="w-1 h-1 bg-[#d9d9d9] rounded-sm"></span>
-          <span className="text-xs text-secondary">{dayjs(data?.post.createdAt).fromNow()}</span>
+          <span className="text-xs text-secondary">{dayjs(post?.post.createdAt).fromNow()}</span>
         </div>
 
         <Link to={`/post/${params.id}`} className="text-xl font-bold mt-1 transition-colors hover:text-primary">
-          {data?.post.title}
+          {post?.post.title}
         </Link>
 
         <article
@@ -58,7 +64,7 @@ export default function SinglePostPage() {
 
         <Like
           postId={params.id!}
-          likesCount={data?.post.reactionsCount}
+          likesCount={post?.post.reactionsCount}
           isLiked={isLikedByCurrentUser}
           className="mt-8"
         />
